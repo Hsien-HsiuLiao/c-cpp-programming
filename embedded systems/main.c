b048c764e238dae1046341984308a1d0889fa792
Hsien-Hsiu <hsien-hsiu.liao@yandex.com>
2020-08-19 08:01:16 -0700

    Update main.c

--- embedded systems/main.c
+++ embedded systems/main.c
@@ -8,9 +8,12 @@
 // a #include tag
 #include <stdio.h>  // Diamond braces for sys lib: Standard I/O
 #include "uart.h"   // Quotes for user lib: UART lib
+// global variable
+unsigned long error;
 
 // Function Prototypes
 // Compiler aid for "type checking"
+void Initialize(void);
 unsigned long Calc_Area(unsigned long s);   // Says Calc_Area expects
                     // an unsigned long and returns an unsigned long
 
@@ -21,22 +24,46 @@ int main(void) {
   unsigned long area;   // size squared meters
   
   UART_Init();  // call subroutine to init uart lib
+  Initialize(); //invoke function by calling it
+  
   printf("This program calculates the area of a square shaped room\n");
   
   side = 3;   // 3 meters
   area = Calc_Area(side);   // square meters
   
   printf("Area of room with side %ld m is %ld sq m\n", side, area); 
+
+  
+  while(1){
+    side = side+2;
+    area = Calc_Area(side);   // square meters
+    if(area !=0 ){
+      printf("Area of room with side %ld m is %ld sq m\n", side, area);
+    }
   }
 
+}
+
+//Initialize global
+//Inputs: none
+//Outputs: none
+//Notes:
+void Initialize(void){
+  error = 0;
+}
 // Calculates area
 // Input: side of a room (unsigned long)
 // Output: area of the room (unsigned long)
 // Notes:
 unsigned long Calc_Area(unsigned long s) {
   unsigned long result;
-  
-  result = s*s;
+  if(s <= 25) {
+    result = s*s;
+  }
+  else{
+    result = 0;   //mistake
+    error = error +1;
+  }
   
   return (result);
   
