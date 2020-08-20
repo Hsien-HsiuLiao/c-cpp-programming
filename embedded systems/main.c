c6d16c42e8297515208a120c74ba61620482a3ba
Hsien-Hsiu <hsien-hsiu.liao@yandex.com>
2020-08-20 08:40:30 -0700

    Update main.c

--- embedded systems/main.c
+++ embedded systems/main.c
@@ -1,13 +1,15 @@
-//**** Documentation Section
+//**** 0. Documentation Section
 // This program calculates the area of a square shaped room
 // Author:
 // Date:
 
-// Pre-processor Directives section
+// 1. Pre-processor Directives section
 // INCLUDES: Lists any external libraries we will refer to using
 // a #include tag
 #include <stdio.h>  // Diamond braces for sys lib: Standard I/O
 #include "uart.h"   // Quotes for user lib: UART lib
+
+// 2. Global declarations section
 // global variable
 unsigned long error;
 
@@ -17,7 +19,7 @@ void Initialize(void);
 unsigned long Calc_Area(unsigned long s);   // Says Calc_Area expects
                     // an unsigned long and returns an unsigned long
 
-// Subroutines section
+// 3. Subroutines section
 // MAIN: Mandatory subroutine for a C program to be executable
 int main(void) {
   unsigned long side;   // room wall in meters
@@ -28,19 +30,25 @@ int main(void) {
   
   printf("This program calculates the area of a square shaped room\n");
   
-  side = 3;   // 3 meters
-  area = Calc_Area(side);   // square meters
-  
-  printf("Area of room with side %ld m is %ld sq m\n", side, area); 
+//  side = 3;   // 3 meters
+//  area = Calc_Area(side);   // square meters 
+//  printf("Area of room with side %ld m is %ld sq m\n", side, area); 
 
   
-  while(1){
-    side = side+2;
+  while(side != 0){
+//    side = side+2;
+    printf("Give room side(zero to quit):");
+    scanf("%ld", &side);
+    
     area = Calc_Area(side);   // square meters
     if(area !=0 ){
       printf("Area of room with side %ld m is %ld sq m\n", side, area);
+    } else {
+        printf("\n Size cannot exceed 25 meters \n");
     }
   }
+  
+  printf("Goodbye. (mistake count = %ld) \n", error);
 
 }
 
